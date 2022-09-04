d7af94cfbc79b415b47dd8d07466462ae5078b31
ohyoungkyu <come8908@gmail.com>
2022-09-04 09:54:16 +0900

    로그인 기능 구현

--- src/main/java/com/oyk/exam/jpaBoard/Controller/UserController.java
+++ src/main/java/com/oyk/exam/jpaBoard/Controller/UserController.java
@@ -15,6 +15,33 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @RequestMapping("/doLogin")
+    @ResponseBody
+    public String doLogin(String email, String password) {
+
+        if (email == null || email.trim().length() == 0) {
+            return "이메일을 입력해주세요.";
+        }
+        email = email.trim();
+
+        User user = userRepository.findByEmail(email).get();
+
+        if(user == null) {
+            return "존재하지 않는 이메일 입니다.";
+        }
+
+        if (password == null || password.trim().length() == 0) {
+            return "비밀번호를 입력해주세요.";
+        }
+        password = password.trim();
+
+        if(user.getPassword().equals(password) == false) {
+            return "비밀번호가 맞지 않습니다. 다시 입력해주세요.";
+        }
+
+        return "%s님 환영합니다.".formatted(user.getName());
+    }
+
     @RequestMapping("/doJoin")
     @ResponseBody
     public String doJoin(String name, String email, String password) {
