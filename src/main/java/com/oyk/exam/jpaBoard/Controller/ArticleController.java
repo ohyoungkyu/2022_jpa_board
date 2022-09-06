308e2f491267f1643a5625bdab309a5307de4fa5
ohyoungkyu <come8908@gmail.com>
2022-09-06 19:27:27 +0900

    게시물 수정 폼 처리 후 자바스크립트로 이동

--- src/main/java/com/oyk/exam/jpaBoard/Controller/ArticleController.java
+++ src/main/java/com/oyk/exam/jpaBoard/Controller/ArticleController.java
@@ -54,7 +54,12 @@ public class ArticleController {
 
         articleRepository.save(article);
 
-        return "%d번 게시물이 수정되었습니다.".formatted(id);
+        return """
+                <script>
+                alert('%d번 게시물이 수정되었습니다.');
+                location.replace('detail?id=%d');
+                </script>
+                """.formatted(article.getId(), article.getId());
     }
 
     @RequestMapping("/doDelete")
